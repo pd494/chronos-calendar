9fa78cf1129adc6ac87dffb5828fa3c64c88324e
pd494 <63752034+pd494@users.noreply.github.com>
2026-02-07 10:43:39 -0800

    remove mobile entry point and mobile crate types

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -8,7 +8,6 @@ fn focus_main_window(app: &tauri::AppHandle) {
   }
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
